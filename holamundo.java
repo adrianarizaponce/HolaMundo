d320efabb80a4dc57cec029a83328c76620d12d6
adrianarizaponce <150147400+adrianarizaponce@users.noreply.github.com>
2023-11-14 14:04:36 +0100

    traduccion

--- holamundo.java
+++ holamundo.java
@@ -3,7 +3,7 @@ public class HolaMundo{
  public static void main (String[]args) {
 
   System.out.println("*".repeat(10));
-    System.out.println("Hola Mundo");
+    System.out.println("Hello world");
    System.out.println("*".repeat(10));
 
 
